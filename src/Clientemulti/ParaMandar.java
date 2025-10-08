aafc1f4e282964c6fff7a87ca518de2d01692366
Alejandra Higuera <al4433205@gmail.com>
2025-10-08 07:13:40 -0700

    corregi un error en ParaMandar

--- src/Clientemulti/ParaMandar.java
+++ src/Clientemulti/ParaMandar.java
@@ -1,13 +1,14 @@
 package Clientemulti;
-
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+
 public class ParaMandar implements Runnable{
     final BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
     final DataOutputStream salida ;
+
     public ParaMandar(Socket s) throws IOException {
         this.salida = new DataOutputStream(s.getOutputStream());
     }
@@ -20,9 +21,9 @@ public class ParaMandar implements Runnable{
                 mensaje = teclado.readLine();
                 salida.writeUTF(mensaje);
             } catch (IOException ex) {
+                System.err.println("Error de conexión al enviar mensaje. Terminando.");
+                break;
             }
-
         }
     }
-
 }
