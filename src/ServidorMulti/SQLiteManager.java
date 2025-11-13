aab59567044a9acf53280287bad2e3bd501f5268
Alejandra Higuera <al4433205@gmail.com>
2025-11-12 20:18:21 -0700

    clase para el manejador de la base de datos sqlite

---
+++ src/ServidorMulti/SQLiteManager.java
@@ -0,0 +1,42 @@
+package ServidorMulti;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQLiteManager {
+    private static final String URL = "jdbc:sqlite:server_data.db";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
+    public static void inicializarDB() {
+        try (Connection conn = getConnection();
+             Statement stmt = conn.createStatement()) {
+
+            // Tabla de Usuarios
+            stmt.execute("CREATE TABLE IF NOT EXISTS Usuarios (" +
+                    "usuario TEXT PRIMARY KEY NOT NULL," +
+                    "password TEXT NOT NULL)");
+
+            //  Tabla de Bloqueos
+            stmt.execute("CREATE TABLE IF NOT EXISTS Bloqueos (" +
+                    "bloqueador TEXT NOT NULL," +
+                    "bloqueado TEXT NOT NULL," +
+                    "PRIMARY KEY (bloqueador, bloqueado))");
+
+            // Tabla de Rankings
+            stmt.execute("CREATE TABLE IF NOT EXISTS Rankings (" +
+                    "usuario TEXT PRIMARY KEY NOT NULL," +
+                    "puntos INTEGER DEFAULT 0," +
+                    "victorias INTEGER DEFAULT 0," +
+                    "derrotas INTEGER DEFAULT 0," +
+                    "empates INTEGER DEFAULT 0)");
+
+            System.out.println("Base de datos SQLite inicializada correctamente.");
+
+        } catch (SQLException e) {
+            System.err.println("Error al inicializar la base de datos: " + e.getMessage());
+        }
+    }
+}
